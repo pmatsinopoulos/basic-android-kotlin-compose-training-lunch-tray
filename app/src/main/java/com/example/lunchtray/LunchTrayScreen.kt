b656b1d36a483a97818298328bab8c23ad1e710b
Panos Matsinopoulos <panos@mixlr.com>
2023-08-30 15:24:42 +0300

    Added Application Top Bar for navigation

--- app/src/main/java/com/example/lunchtray/LunchTrayScreen.kt
+++ app/src/main/java/com/example/lunchtray/LunchTrayScreen.kt
@@ -19,17 +19,27 @@ import android.content.Context
 import android.content.Intent
 import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.example.lunchtray.datasource.DataSource
 import com.example.lunchtray.ui.AccompanimentMenuScreen
@@ -47,7 +57,32 @@ enum class LunchTrayScreen(@StringRes val title: Int) {
     Checkout(title = R.string.order_checkout)
 }
 
-// TODO: AppBar
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun LunchTrayAppBar(
+    currentScreen: LunchTrayScreen,
+    canNavigateBack: Boolean,
+    navigateUp: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    TopAppBar(
+        title = { Text(stringResource(id = currentScreen.title)) },
+        colors = TopAppBarDefaults.mediumTopAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer
+        ),
+        modifier = modifier,
+        navigationIcon = {
+            if (canNavigateBack) {
+                IconButton(onClick = navigateUp) {
+                    Icon(
+                        imageVector = Icons.Filled.ArrowBack,
+                        contentDescription = stringResource(R.string.back_button)
+                    )
+                }
+            }
+        }
+    )
+}
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -57,11 +92,21 @@ fun LunchTrayApp(
     viewModel: OrderViewModel = viewModel()
 ) {
     val uiState by viewModel.uiState.collectAsState()
+    val backStackEntry by navController.currentBackStackEntryAsState()
+
     val context = LocalContext.current
+    val currentScreen = LunchTrayScreen.valueOf(
+        backStackEntry?.destination?.route ?: LunchTrayScreen.StartOrder.name
+    )
+    val canNavigateBack = navController.previousBackStackEntry != null
 
     Scaffold(
         topBar = {
-            // TODO: AppBar
+            LunchTrayAppBar(
+                currentScreen = currentScreen,
+                canNavigateBack = canNavigateBack,
+                navigateUp = { navController.navigateUp() }
+            )
         }
     ) { innerPadding ->
         NavHost(
@@ -148,7 +193,10 @@ private fun shareOrder(
         action = Intent.ACTION_SEND
         type = "text/plain"
         putExtra(Intent.EXTRA_SUBJECT, R.string.order_summary)
-        putExtra(Intent.EXTRA_TEXT, "${viewModel.uiState.value.entree?.name}\n${viewModel.uiState.value.sideDish?.name}\n${viewModel.uiState.value.accompaniment?.name}")
+        putExtra(
+            Intent.EXTRA_TEXT,
+            "${viewModel.uiState.value.entree?.name}\n${viewModel.uiState.value.sideDish?.name}\n${viewModel.uiState.value.accompaniment?.name}"
+        )
     }
     context.startActivity(
         Intent.createChooser(
